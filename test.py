Analysis now runs but fits_mods is scaling wrong, needs working on

test.py
@@ -1,7 +1,19 @@
 import numpy as np
+from astropy.io import fits
 
-from astropy import units as u
+import matplotlib.pyplot as plt
 
-arr = np.arange(1,10,1)*u.Unit('m s-1')
+with fits.open("./dr16/sdss/spectro/redux/26/spectra/0854/spec-0854-52373-0369-0.9.fits", comments='#') as hdul:
+            header = hdul[0].header
+            spectrum_data = hdul[1].data
 
-print(type(arr.value))
+#Assumes the spectrum data is in the format:
+#Wavelength, Flux, FluxError
+
+wave = np.array(spectrum_data['loglam'])
+flux = np.array(spectrum_data['flux'])
+
+fig, ax = plt.subplots()
+ax.plot(wave,flux)
+
+plt.show()